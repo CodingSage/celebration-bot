0902a432a2633c7ff39d3076b18e0a46b883e9e9
Vinayak Karuppasamy <vinayak.rain@gmail.com>
2021-10-23 20:50:35 -0400

    fixed sentence generation

--- celebration.py
+++ celebration.py
@@ -2,9 +2,7 @@ import nltk
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 from nltk import pos_tag, word_tokenize, RegexpParser
-
 from queue import Queue
-
 import random
 
 chunker = RegexpParser("""
@@ -29,54 +27,38 @@ greetings = [
     "Hi"
 ]
 
+# TODO update this implementation and use a parse tree instead
+
 def celebrate(user, text):
     tagged = _get_pos_tags(text)
-    (nouns, verbs) = _extract_nouns_verbs(tagged)
-    return _generate_sentence(user, nouns, verbs)
+    (nouns, verbs, actionnouns) = _extract_nouns_verbs(tagged)
+    return _generate_sentence(user, nouns, verbs, actionnouns)
 
 def _extract_nouns_verbs(tagged):
     nouns = []
     verbs = []
+    actionnouns = []
+    verbs_encountered = False
     for tag in tagged:
-        if tag[1][0] == 'N':
+        if tag[1][0] == 'N' and not verbs_encountered:
             nouns.append(tag[0])
+        elif tag[1][0] == 'N' and verbs_encountered:
+            actionnouns.append(tag[0])
         elif tag[1][0] == 'V':
             verbs.append(tag[0])
+            verbs_encountered = True
     
-    return (nouns, verbs)
+    return (nouns, verbs, actionnouns)
 
-def _generate_sentence(user, nouns, verbs):
+def _generate_sentence(user, nouns, verbs, actionnouns):
     name = user if len(nouns) == 0 else ', '.join(nouns)
     exclamation_index = random.randint(0, len(exclamations)-1)
     exclaim = exclamations[exclamation_index]
-    action = "good going with the " + ', '.join(verbs)
-    return exclaim + "!" + name + " " + action + "!"
+    action = "good going with " + ', '.join(verbs) + " the " + ", ".join(actionnouns)
+    return exclaim + "! " + name + " " + action + "!"
 
 def _get_pos_tags(text):
     return pos_tag(word_tokenize(text))
-
-def _parse_text(text):
-    tagged = _get_pos_tags(text)
-    parse_tree = chunker.parse(tagged)
-    print(parse_tree)
-    return parse_tree
-
-def _parse_nouns_verbs(tree):
-    nouns = []
-    verbs = []
-
-    visited = set()
-    q = Queue()
-    q.put(tree[0])
-
-    # TODO complete this
-    while not q.empty():
-        node = q.get()
-        visited.add(node)
-        if node.label()[0] == 'N':
-            nouns.append(node.label())
-
-    return (nouns, verbs)
 
 def run_tests():
     tests = [
